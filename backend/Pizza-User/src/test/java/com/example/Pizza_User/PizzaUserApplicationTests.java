144eae4ef991c6e440c422c540fb633d4c125781
Amod Matheesha <145131467+AmodMatheesha2003@users.noreply.github.com>
2024-09-10 17:26:35 +0530

    Add files via upload

---
+++ backend/Pizza-User/src/test/java/com/example/Pizza_User/PizzaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pizza_User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
